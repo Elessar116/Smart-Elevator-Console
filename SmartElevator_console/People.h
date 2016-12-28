5226ed6458048d172ee0178d7204302a1291268e
vrElessar <N16054572@mail.ncku.edu.tw>
2016-12-29 01:56:44 +0800

    init people

--- SmartElevator_console/People.h
+++ SmartElevator_console/People.h
@@ -1,5 +1,17 @@
 #ifndef PEOPLE_H
 #define PEOPLE_H
 
+class People
+{
+private:
+	bool isInBuilding;
+	int currentFloor;
+	int toFloor;
+	int waitElevTime;
+	int inElevTime;
+public:
+	People();
+	~People();
+};
 
 #endif
